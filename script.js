252e24b9dc6b57f7afe1b497cdee86972f165e2e
Amara <115988065+vilandra-coder@users.noreply.github.com>
2023-04-10 07:10:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+const billInput = document.getElementById('bill_input')
+const tips = document.querySelectorAll('.tips')
+const peopleInput = document.getElementById('people-num')
+const errorMessage = document.querySelector('.errmsg')
+const perPersonAmount = document.getElementById('perperson-amt')
+const totalPerPerson = document.getElementById('totalper-person')
+const reset = document.querySelector('.reset')
+const tipInput = document.querySelector('.tip-input')
+
+
+
+
+console.log(reset)
